bcad00986a8f5c1ee7453e18750e102c32e103ca
christopher <christopher.arthaud@gmail.com>
2024-05-18 15:27:04 +0200

    first commit
    first commit

---
+++ src/main/java/com/reservo/reservoback/ReservoBackApplication.java
@@ -0,0 +1,13 @@
+package com.reservo.reservoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservoBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/reservo/reservoback/model/CustomerModel.java
@@ -0,0 +1,4 @@
+package com.reservo.reservoback.model;
+
+public class CustomerModel {
+}
